df601ab5727051ef317d682ddf34b21a16fa11dd
Peter Malcolm <peter.t.malcolm@gmail.com>
2016-10-21 16:09:50 -0400

    stop the recursive reverse!!!

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var through = require('through2');
 stream.pipe(split()).pipe(through(function (line, enc, next) {
   var lines = loop.state.lines;
   lines.push(line.toString());
-  loop.update({ lines: lines.reverse() });
+  loop.update({ lines: lines });
   //loop.update({ lines: lines.slice(-10) }); // keep only the 10 most recent lines
   next()
 }));
